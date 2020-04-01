2e1eb206e489710e74fa526577424ab29a14ff4e
JohnNWarila <johnwarila@gmail.com>
2020-04-01 10:13:54 -0700

    Added mock client dir

---
+++ mock-client/src/App.js
@@ -0,0 +1,14 @@
+/** @jsx jsx */
+import { jsx, css } from '@emotion/core'
+
+function App() {
+  return (
+    <div style={css`
+      margin: 0 auto; 
+    `}>
+      
+    </div>
+  );
+}
+
+export default App;
